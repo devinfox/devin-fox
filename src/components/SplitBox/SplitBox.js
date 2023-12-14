636dea91dea6f4f1135b494efd17f0c113d05b12
Devin <devinfoxbusiness@gmail.com>
2023-12-14 14:58:27 -0800

    Updated everything

--- src/components/SplitBox/SplitBox.js
+++ src/components/SplitBox/SplitBox.js
@@ -14,7 +14,7 @@ const SplitBox = () => {
       <div className="split-box-item item-2">
         <h2 className="second-header" style={{paddingLeft: "50px", paddingRight: "50px"}}>Marketing Services</h2>
         <img class="glow-image" src={adwords} alt="Image 2" />
-        <button class="click-here" href="/results">Results</button>
+        <button class="click-here"><a class="button-1" href="/results">Results</a></button>
       </div>
     </div>
   );
